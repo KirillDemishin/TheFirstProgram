70b6fdad164442ae840a7f55dd34677693594c1f
Kirill <kirilshou@gmail.com>
2023-07-31 15:17:30 +0800

    персональное имя

---
+++ exampiers005_nameYou/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "кирилл")
+{
+    Console.WriteLine("Приветствую вас господин!");
+}
+else
+{
+    Console.Write("Првет, ");
+    Console.WriteLine(username);
+}
